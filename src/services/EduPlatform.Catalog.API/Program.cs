f029d2f8fbabc891c99707f3ebc4a7fc3d2dc183
tahsintiryaki <tahsin14tiryaki@gmai.com>
2025-12-27 21:21:54 +0300

    mediatr configuration was added

--- src/services/EduPlatform.Catalog.API/Program.cs
+++ src/services/EduPlatform.Catalog.API/Program.cs
@@ -1,19 +1,24 @@
+using EduPlatform.Catalog.API;
 using EduPlatform.Catalog.API.Features.Categories;
 using EduPlatform.Catalog.API.Options;
 using EduPlatform.Catalog.API.Repositories;
+using EduPlatform.Shared.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//mongodb connection value  validate
-builder.Services.AddOptionExt();
-//connect mongodb  
-builder.Services.AddMongoDbConfigureExt();
+builder.Services.AddOptionExt();//mongodb connection value  validate
+builder.Services.AddMongoDbConfigureExt();//connect mongodb  
+builder.Services.AddCommonServiceExt(typeof(CatalogAssembly)); //
+
+
 var app = builder.Build();
 
+
 app.AddCategoryGroupEndpointExt();
+
 
 if (app.Environment.IsDevelopment())
 {
---
+++ src/shared/EduPlatform.Shared/Extensions/CommonServiceExt.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EduPlatform.Shared.Extensions;
+
+public static class CommonServiceExt
+{
+    public static IServiceCollection AddCommonServiceExt(this IServiceCollection services, Type assembly)
+    {
+        services.AddHttpContextAccessor();
+        services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining(assembly));
+        return services;
+    }
+}
