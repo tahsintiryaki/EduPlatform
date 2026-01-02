abf16ad221890b9a70bf3c588631933b5e62749d
tahsintiryaki <tahsin14tiryaki@gmai.com>
2026-01-02 16:00:09 +0300

    AppDbContext was added

---
+++ src/services/EduPlatform.Order.Persistence/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿#region
+
+using EduPlatform.Order.Domain.Entities;
+using EduPlatform.Order.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+#endregion
+
+namespace EduPlatform.Order.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Domain.Entities.Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderConfiguration).Assembly);
+
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/services/EduPlatform.Order.Persistence/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,21 @@
+﻿#region
+
+using EduPlatform.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+#endregion
+
+namespace EduPlatform.Order.Persistence.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).UseIdentityColumn();
+        builder.Property(x => x.ProductId).IsRequired();
+        builder.Property(x => x.ProductName).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.UnitPrice).HasColumnType("decimal(18,2)").IsRequired();
+    }
+}
