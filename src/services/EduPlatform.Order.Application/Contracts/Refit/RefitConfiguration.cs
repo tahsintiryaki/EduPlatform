683f53cb440105f0f3711e156672fc1f30046264
tahsintiryaki <tahsin14tiryaki@gmai.com>
2026-01-12 14:38:51 +0300

    request with client credential  method was prepared

--- src/services/EduPlatform.Order.Application/Contracts/Refit/RefitConfiguration.cs
+++ src/services/EduPlatform.Order.Application/Contracts/Refit/RefitConfiguration.cs
@@ -2,6 +2,7 @@ using EduPlatform.Order.Application.Contracts.Refit.PaymentService;
 using EduPlatform.Shared.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Refit;
 
 namespace EduPlatform.Order.Application.Contracts.Refit;
@@ -10,6 +11,15 @@ public static class RefitConfiguration
 {
     public static IServiceCollection AddRefitConfigurationExt(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddOptions<IdentityOption>().BindConfiguration(nameof(IdentityOption)).ValidateDataAnnotations()
+            .ValidateOnStart();
+        services.AddSingleton<IdentityOption>(sp => sp.GetRequiredService<IOptions<IdentityOption>>().Value);
+
+        services.AddOptions<ClientSecretOption>().BindConfiguration(nameof(ClientSecretOption)).ValidateDataAnnotations()
+            .ValidateOnStart();
+        services.AddSingleton<ClientSecretOption>(sp => sp.GetRequiredService<IOptions<ClientSecretOption>>().Value);
+
+        
         //added interceptor for token 
         services.AddScoped<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IPaymentService>().ConfigureHttpClient(configure =>
