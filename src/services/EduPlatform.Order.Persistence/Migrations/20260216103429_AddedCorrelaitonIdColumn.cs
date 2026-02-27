1005ecbff50e7c989412bd4981da3ef20793680d
tahsintiryaki <tahsin14tiryaki@gmai.com>
2026-02-27 12:10:52 +0300

    migration files

---
+++ src/services/EduPlatform.Order.Persistence/Migrations/20260216103429_AddedCorrelaitonIdColumn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduPlatform.Order.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCorrelaitonIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CorrelationId",
+                table: "OrderOutboxes",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrelationId",
+                table: "OrderOutboxes");
+        }
+    }
+}
---
+++ src/services/EduPlatform.Payment.API/Migrations/20260216104204_AddedCorrelationIdColumn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduPlatform.Payment.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCorrelationIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CorrelationId",
+                table: "OrderInboxes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrelationId",
+                table: "OrderInboxes");
+        }
+    }
+}
